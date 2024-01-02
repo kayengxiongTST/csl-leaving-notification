76636f5d6862f49028e9db3db66caa6676e94b36
Kayengxiong TONGSUATONG <104555848+kayengxiongTST@users.noreply.github.com>
2024-01-02 11:39:10 +0700

    Revert "windows minimize to system tray"

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
-try { require('electron-reloader')(module); } catch { }
-const { app, BrowserWindow, Tray, Menu, nativeImage } = require('electron')
+try { require('electron-reloader')(module); } catch { };
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-let isQuiting;
-let tray;
-
-app.on('before-quit', function () {
-	isQuiting = true;
-});
-
 function createWindow() {
-	const cslIcon = nativeImage.createFromPath('csl-favicon.ico')
-
 	const win = new BrowserWindow({
 		width: 400,
 		height: 300,
@@ -19,49 +10,9 @@ function createWindow() {
 			preload: path.join(__dirname, 'preload.js')
 		},
 		resizable: false,
-		show: false,
-		icon: cslIcon
 	})
-	win.hide()
+
 	win.loadFile('src/index.html')
-
-	win.once('ready-to-show', () => {
-		win.show()
-	})
-
-	win.on('minimize',function(event){
-		event.preventDefault()
-		win.hide()
-	})
-	
-	win.on('close', function (event) {
-		if(!isQuiting){
-			event.preventDefault()
-			win.hide()
-			event.returnValue = false;
-		}
-	
-		return false
-	})
-
-
-	const contextMenu = Menu.buildFromTemplate([
-		{ label: 'Open CSL App', click:  function(){
-			win.show()
-		} },
-		{ label: 'Quit', click:  function(){
-			isQuiting = true
-			app.quit()
-		} }
-	])
-	tray = new Tray(path.join(__dirname, 'csl-favicon.ico'))
-	tray.setToolTip('CSL leaving notification')
-	tray.setTitle('CSL')
-	tray.setContextMenu(contextMenu)
-	tray.on('double-click', function (event) {
-		win.show()
-	})
-
 }
 
 app.whenReady().then(() => {
